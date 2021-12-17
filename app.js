966865d9d5813aba840744ad07e22fad14fdd7c0
Boltzy50 <90049506+Boltzy50@users.noreply.github.com>
2021-12-16 21:30:56 -0500

    second working commit, working photos

--- app.js
+++ app.js
@@ -6,20 +6,45 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
-const mySongs = require('./db');
+// const img = require('./img');
 
 const server = http.createServer(app);
+const db = require('./db');
+const res = require('express/lib/response');
+app.use(express.static('public'));
 
-
-
-
-
-
-// enable static public folder
-// app.use(express.static('public'));
-
-
-
-//  app.get('/', (req, res)=>{
+// app.get('/', (req, res)=>{
     
-//  })
+// })
+
+app.get('/mySongs', (req, res)=>{
+    let htmlString = ``;
+    //Loop through the array
+    for (let i=0; i<db.length; i++) {   
+        //add an empty list item to htmlString
+        let album = db[i].name
+        htmlString += `<ul>${album}</ul>`;
+    }
+    res.send(`<ul>${htmlString}</ul>`);
+});
+
+app.get('/mySongs/:handle', (req, res)=>{
+    const {handle} = req.params;
+    const test = db.find(f => f.handle === handle);
+    
+    if (test){
+        let htmlData = ``;
+        htmlData += `<h1>${test.name}</h1>`;
+        htmlData += `<img src=${test.imgURL}>`;
+        htmlData += `<h4>Year: ${test.publishDate}</h4>`;
+        htmlData += `<h6>Track List: ${test.songTitles}</h6>`;
+        res.send(htmlData)
+    }else{
+        res.send('No album with that name');
+    }
+});
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
